16253f64a76e01e08d14f78fa554ddb59fde319c
parasojha256 <parasojha256@gmail.com>
2025-04-21 20:10:01 +0545

    init

---
+++ src/Input.js
@@ -0,0 +1,5 @@
+import { jsx as _jsx } from "react/jsx-runtime";
+const Input = () => {
+    return _jsx("div", { children: "Input" });
+};
+export default Input;
---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/Input.tsx'),
+      name: 'MyComponent',
+      fileName: (format) => `input.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
